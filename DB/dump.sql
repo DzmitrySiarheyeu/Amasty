4a68f31afd30cd8db2d2500f592a234058c9ea57
DzmitrySiarheyeu <dzmitry.siarheyeu@gmail.com>
2021-03-21 10:15:10 +0300

    0.1

--- DB/dump.sql
+++ DB/dump.sql
@@ -1,56 +1,184 @@
--- --------------------------------------------------------
--- Хост:                         127.0.0.1
--- Версия сервера:               5.7.23 - MySQL Community Server (GPL)
--- Операционная система:         Win64
--- HeidiSQL Версия:              9.5.0.5196
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Мар 21 2021 г., 08:13
+-- Версия сервера: 10.3.16-MariaDB
+-- Версия PHP: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Дамп данных таблицы testbase.cities: ~5 rows (приблизительно)
-DELETE FROM `cities`;
-/*!40000 ALTER TABLE `cities` DISABLE KEYS */;
+--
+-- База данных: `testbase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `cities`
+--
+
+CREATE TABLE `cities` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Очистить таблицу перед добавлением данных `cities`
+--
+
+TRUNCATE TABLE `cities`;
+--
+-- Дамп данных таблицы `cities`
+--
+
 INSERT INTO `cities` (`id`, `name`) VALUES
-	(1, 'Minsk'),
-	(2, 'Gomel'),
-	(3, 'Hrodna'),
-	(4, 'Baranovichi'),
-	(5, 'Brest'),
-	(6, 'Zhlobin'),
-	(7, 'Vitebsk'),
-	(8, 'Krugloe');
-/*!40000 ALTER TABLE `cities` ENABLE KEYS */;
+(1, 'Minsk'),
+(2, 'Gomel'),
+(3, 'Hrodna'),
+(4, 'Baranovichi'),
+(5, 'Brest'),
+(6, 'Zhlobin'),
+(7, 'Vitebsk'),
+(8, 'Krugloe');
 
--- Дамп данных таблицы testbase.persons: ~4 rows (приблизительно)
-DELETE FROM `persons`;
-/*!40000 ALTER TABLE `persons` DISABLE KEYS */;
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `persons`
+--
+
+CREATE TABLE `persons` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `city_id` bigint(20) UNSIGNED NOT NULL,
+  `fullname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Очистить таблицу перед добавлением данных `persons`
+--
+
+TRUNCATE TABLE `persons`;
+--
+-- Дамп данных таблицы `persons`
+--
+
 INSERT INTO `persons` (`id`, `city_id`, `fullname`) VALUES
-	(1, 5, 'Ivan Petrov'),
-	(2, 3, 'Sebastian Haponenka'),
-	(3, 3, 'Vasil Lutsevich'),
-	(4, 2, 'Leo Klimovich'),
-	(5, 7, 'Matea Kezhman'),
-	(6, 8, 'Alex Marshall'),
-	(7, 3, 'Bilbo Beggins');
-/*!40000 ALTER TABLE `persons` ENABLE KEYS */;
+(1, 5, 'Ivan Petrov'),
+(2, 3, 'Sebastian Haponenka'),
+(3, 3, 'Vasil Lutsevich'),
+(4, 2, 'Leo Klimovich'),
+(5, 7, 'Matea Kezhman'),
+(6, 7, 'Matea Kezhman'),
+(7, 8, 'Alex Marshall'),
+(8, 3, 'Bilbo Beggins');
 
--- Дамп данных таблицы testbase.transactions: ~1 rows (приблизительно)
-DELETE FROM `transactions`;
-/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
-INSERT INTO `transactions` (`transaction_id`, `from_person_id`, `to_person_id`, `amount`) VALUES
-	(1, 4, 2, 10.0000),
-	(2, 2, 3, 7.0000),
-	(3, 5, 2, 12.5400),
-	(4, 4, 7, 13.0000),
-	(5, 3, 1, 8.2300),
-	(6, 1, 3, 12.3000),
-	(7, 2, 5, 3.1200);
-/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+-- --------------------------------------------------------
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Структура таблицы `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `from_person_id` bigint(20) UNSIGNED NOT NULL,
+  `to_person_id` bigint(20) UNSIGNED NOT NULL,
+  `amount` double(4,4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Очистить таблицу перед добавлением данных `transactions`
+--
+
+TRUNCATE TABLE `transactions`;
+--
+-- Дамп данных таблицы `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `from_person_id`, `to_person_id`, `amount`) VALUES
+(1, 4, 2, 0.9999),
+(2, 2, 3, 0.9999),
+(3, 5, 2, 0.9999),
+(4, 4, 7, 0.9999),
+(5, 3, 1, 0.9999),
+(6, 1, 3, 0.9999),
+(7, 2, 5, 0.9999);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `cities`
+--
+ALTER TABLE `cities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `persons`
+--
+ALTER TABLE `persons`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `persons_city_id_foreign` (`city_id`);
+
+--
+-- Индексы таблицы `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `transactions_from_person_id_foreign` (`from_person_id`),
+  ADD KEY `transactions_to_person_id_foreign` (`to_person_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `cities`
+--
+ALTER TABLE `cities`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT для таблицы `persons`
+--
+ALTER TABLE `persons`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT для таблицы `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `persons`
+--
+ALTER TABLE `persons`
+  ADD CONSTRAINT `persons_city_id_foreign` FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_from_person_id_foreign` FOREIGN KEY (`from_person_id`) REFERENCES `persons` (`id`),
+  ADD CONSTRAINT `transactions_to_person_id_foreign` FOREIGN KEY (`to_person_id`) REFERENCES `persons` (`id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
